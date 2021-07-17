262c094147e491cb118cc2d04f288fb0d4b07636
revengeWillBeTaken <69358814+revengeWillBeTaken@users.noreply.github.com>
2021-07-17 10:17:01 +0530

    Update 344.cpp

--- Recursion/344.cpp
+++ Recursion/344.cpp
@@ -1,1 +1,14 @@
-
+void recurse(vector<char>&s,int start,int end)
+{
+    swap(s[start],s[end]);
+    if(start+1<end-1)
+    recurse(s,start+1,end-1);
+}
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int n=s.size();
+        if(0<n-1)
+            recurse(s,0,n-1);
+    }
+};
