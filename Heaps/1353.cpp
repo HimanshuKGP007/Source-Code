0ee16481233a964ac613f74e6e2bd8054f4b1668
hithuman <87515067+hithuman@users.noreply.github.com>
2021-07-20 12:30:04 +0530

    Update 1353.cpp

--- Heaps/1353.cpp
+++ Heaps/1353.cpp
@@ -57,3 +57,44 @@ public:
         return ans;
     }
 };
+
+
+//////////////////////////// CODE USING SET & LOWERBOUND
+class Compare
+{
+public:
+    bool operator()(pair<int,int> a, pair<int,int> b){
+        if(a.second==b.second)return a.first<b.first;
+        return a.second>b.second;
+    }
+};
+
+class Solution {
+public:
+    int maxEvents(vector<vector<int>>& events) {
+        priority_queue<pair<int,int>, vector<pair<int, int>>,Compare> pq;
+        int maxi;
+        for(int i=0; i<events.size(); i++){
+            maxi = max(maxi,events[i][1]);
+            pq.push(make_pair(events[i][0], events[i][1]));
+        }
+        
+        int ans=0;
+       // vector<bool> vis(100001, false);
+        set<int> s;
+        for(int i=1;i<=maxi;i++){
+            s.insert(i);
+        }
+        while(!pq.empty()){
+            pair<int,int> curr = pq.top();
+            pq.pop();
+            auto low = s.lower_bound(curr.first);
+            if(low==s.end())continue;
+            if((*low)<=curr.second){
+                s.erase(*low);
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
