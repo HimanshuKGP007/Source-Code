e186a24b540e441fc2d9f805efed97afc9659f98
anshuman30 <53341556+anshuman30@users.noreply.github.com>
2021-07-21 11:45:57 +0530

    Update 636.cpp

--- Heaps/636.cpp
+++ Heaps/636.cpp
@@ -92,3 +92,61 @@ public:
         return ans;
     }
 };
+
+// Anshuman Solution
+
+class Solution {
+public:
+    void print_stk(stack<pair<int,int>> s){
+        cout<<"Printing stack\n";
+        while(!s.empty()){
+            cout<<s.top().first<<" "<<s.top().second<<"\n";
+            s.pop();
+        }
+    }
+    vector<int> exclusiveTime(int n, vector<string>& logs) {
+        vector<int> ans(n,0);
+        stack<pair<int,int>> process_stack;
+        for(int i=0;i<logs.size();i++){
+            int j=0;
+            while(logs[i][j]!=':'){
+                j++;
+            }
+            string pid=logs[i].substr(0,j);
+            int k=j+1;
+            while(logs[i][k]!=':'){
+                k++;
+            }
+            string sOrE=logs[i].substr(j+1,k-j-1);
+            string tim=logs[i].substr(k+1,logs[i].size()-1-k);
+            stringstream stream1(pid);
+            int proc_num=0;
+            stream1>>proc_num;
+            // cout<<proc_num<<" ";
+            stringstream stream2(tim);
+            int timestamp=0;
+            stream2>>timestamp;
+            // cout<<timestamp<<" ";
+            // cout<<sOrE<<"\n";
+            if(sOrE.compare("start")==0){
+                if(process_stack.empty()){
+                    process_stack.push(make_pair(proc_num,timestamp));
+                }
+                else{
+                    ans[process_stack.top().first]+=timestamp-process_stack.top().second;
+                    process_stack.push(make_pair(proc_num,timestamp));
+                }
+            }
+            else if(sOrE.compare("end")==0){
+                if(!process_stack.empty()){
+                    ans[process_stack.top().first]+=timestamp-process_stack.top().second+1;
+                    process_stack.pop();
+                    if(!process_stack.empty())
+                        process_stack.top().second=timestamp+1;
+                }
+            }
+            //print_stk(process_stack);
+        }
+        return ans;
+    }
+};
