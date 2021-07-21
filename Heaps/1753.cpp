b766205407cae6155768ec64139bc0cba6924e03
anshuman30 <53341556+anshuman30@users.noreply.github.com>
2021-07-21 12:00:07 +0530

    Update 1753.cpp

--- Heaps/1753.cpp
+++ Heaps/1753.cpp
@@ -24,3 +24,25 @@ public:
         return ans;
     }
 };
+
+// Anshuman Solution O(1)
+
+class Solution {
+public:
+    int maximumScore(int a, int b, int c) {
+        vector<int> v;
+        v.push_back(a);
+        v.push_back(b);
+        v.push_back(c);
+        sort(v.begin(),v.end());
+        int ans=v[2];
+        if(v[0]+v[1]>=v[2]){
+            int extra=(v[0]+v[1]-v[2])/2;
+            ans+=extra;
+        }
+        else{
+            ans=v[0]+v[1];
+        }
+        return ans;
+    }
+};
