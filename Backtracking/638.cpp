5d1a0d7bb26707bc2dfc8bd1b712422eea17dcf4
hithuman <87515067+hithuman@users.noreply.github.com>
2021-07-27 12:01:44 +0530

    Update 638.cpp

--- Backtracking/638.cpp
+++ Backtracking/638.cpp
@@ -1,3 +1,47 @@
+\\\\\\\\\\\12ms\\\\\\\\\\\88%fatser\\\\\\\\\\\\\\\
+
+class Solution {
+public:
+    
+    map<vector<int>, int> m;                                // map to store vector of needs and it scorresponding minimum cost
+    
+    int helper(vector<int>& price, vector<vector<int>>& special, vector<int>& needs, map<vector<int>, int> &m){
+        if(m.find(needs)!=m.end())return m[needs];
+        int minimum = INT_MAX/2;                            //putting INT_MAX may make it ot of bounds
+        int n = needs.size();
+        
+        int maxi = *max_element(needs.begin(), needs.end());
+        if(maxi==0)return 0;                                // checking if all zero;
+        
+        for(int i=0; i<special.size(); i++){
+            int minim=INT_MAX;
+            for(int j=0; j<n; j++){                         // checking if minimum value in needs is less than 0, then we won't consider the case
+                needs[j] = needs[j]-special[i][j];
+                minim = min(minim, needs[j]);
+            }
+            
+            if(minim>=0)minimum = min(minimum, special[i][n]+helper(price, special, needs,m));
+            for(int j=0; j<n; j++){
+                needs[j] = needs[j]+special[i][j];
+            }
+        }
+        int price_no_offer = 0;                             //price if no offer was used
+        for( int i=0; i<n; i++){
+            if(needs[i]!=0)price_no_offer+=needs[i]*price[i];
+        }
+        minimum = min(minimum, price_no_offer);
+        m[needs]=minimum;                                   // update in map
+        return minimum;
+    }
+    
+    int shoppingOffers(vector<int>& price, vector<vector<int>>& special, vector<int>& needs) {
+        m.clear();
+        return helper(price,special, needs,m);
+    }
+};
+
+\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
 #define pb push_back
 #define mp make_pair
 #define fi first
